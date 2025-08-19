8ff24a196cf897f15845f7e561c6396966b7b2f7
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-08-19 01:55:54 -0300

    Header: Frete grátis

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <header>
+            <div className="bg-black text-white text-center p-4">
+                <strong>FRETE GRÁTIS</strong> para todo o Nordeste nas compras acima de R$ 199,00. <strong>APROVEITA!</strong>
+            </div>
+            ...
+        </header>
+    );
+}
