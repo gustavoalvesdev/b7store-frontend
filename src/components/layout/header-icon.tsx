30ae5e3e4d1c3fda2037f3e581f3bd795b7a6226
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-08-19 17:24:37 -0300

    Header: Menu Mobile

--- src/components/layout/header-icon.tsx
+++ src/components/layout/header-icon.tsx
@@ -3,19 +3,32 @@ import Image from "next/image";
 type Props = {
     src: string;
     alt: string;
+    selected?: boolean;
+    srcSelected?: string;
 };
 
-export const HeaderIcon = ({src, alt}: Props) => {
+export const HeaderIcon = ({src, alt, selected, srcSelected}: Props) => {
 
     return (
-        <div className="size-12 border border-gray-200 rounded-sm flex justify-center items-center hover:bg-gray-100">
-            <Image 
-                src={src}
-                alt={alt}
-                width={24}
-                height={24}
-                className="size-6"
-            />
+        <div className={`size-12 border border-gray-200 rounded-sm flex justify-center items-center  ${selected ? 'bg-blue-600' : 'hover:bg-gray-100'}`}>
+            {!selected &&
+                <Image 
+                    src={src}
+                    alt={alt}
+                    width={24}
+                    height={24}
+                    className="size-6"
+                />
+            }
+            {selected && srcSelected &&
+                <Image 
+                    src={srcSelected}
+                    alt={alt}
+                    width={24}
+                    height={24}
+                    className="size-6"
+                />
+            }
         </div>
     );
 
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import { HeaderIcon } from "./header-icon";
 import Link from "next/link";
+import { useState } from "react";
+
+type MenuItem = {
+    label: string;
+    href: string;
+};
 
 export function Header() {
+
+    const menu: MenuItem[] = [
+        { label: 'Camisa', href: '/categories/camisa' },
+        { label: 'Kits', href: '/categories/kits' }
+    ];
+
+    const [menuOpened, setMenuOpened] = useState(false);
+
     return (
         <header className="bg-white border-b border-gray-200">
             <div className="bg-black text-white text-center p-4">
@@ -11,12 +27,14 @@ export function Header() {
             <div className="w-full max-w-6xl mx-auto p-6">
                 <div className="flex items-center justify-between">
                     <div>
+                        <Link href={'/'}>
                         <Image
                             src="/assets/ui/logo-black.png" 
                             alt="B7Store"
                             width={120}
                             height={40}
-                        />                  
+                        />    
+                        </Link>              
                     </div>
                     <div className="flex gap-4">
                         <Link href={'/my-orders'}>
@@ -25,11 +43,36 @@ export function Header() {
                         <Link href={'/cart'}>
                             <HeaderIcon src="/assets/ui/shopping-bag-4-line.png" alt="Carrinho" />
                         </Link>
-                        <div className="md:hidden">
-                            <HeaderIcon src="/assets/ui/menu-line.png" alt="Menu" />
+                        <div className="md:hidden" onClick={() => setMenuOpened(!menuOpened)}>
+                            <HeaderIcon 
+                                src="/assets/ui/menu-line.png" 
+                                alt="Menu" 
+                                selected={menuOpened}
+                                srcSelected="/assets/ui/menu-line-white.png"
+                            />
                         </div>
                     </div>
                 </div>
+            </div>
+            {menuOpened &&
+                <div className="md:hidden">
+                    {menu.map(item => (
+                        <Link key={item.label} href={item.href}>
+                            <div className="p-6 border-b border-gray-200 flex items-center justify-between">
+                                <div className="font-medium text-lg text-gray-500">{item.label}</div>
+                                    <Image
+                                        src={'/assets/ui/arrow-up-right.png'}
+                                        alt='Ir à categoria'
+                                        width={24}
+                                        height={24}
+                                    />
+                            </div>
+                        </Link>
+                    ))};
+                </div>
+            }
+            <div className="p-6 md:hidden">
+                Busca Mobile
             </div>
         </header>
     );
